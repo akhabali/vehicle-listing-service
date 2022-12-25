083c51d651244fc13754da1fd0d75a0208e47cd4
Anas Khabali <anas.khabali@gmail.com>
2022-12-25 23:14:43 +0100

    Init spring project with initial dependencies.
    The project structure and it initial dependencies were generated using https://start.spring.io/
    
    This versioin requires JDK 17

---
+++ src/test/java/io/github/akhabali/VehicleListingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.akhabali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleListingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
