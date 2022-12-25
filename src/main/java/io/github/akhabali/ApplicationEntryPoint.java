9f48f6133aa1ed87b24f57ccb7b1a1d27986f6c1
Anas Khabali <anas.khabali@gmail.com>
2022-12-25 23:14:43 +0100

    Init spring project with initial dependencies.
    The project structure and it initial dependencies were generated using https://start.spring.io/
    
    This versioin requires JDK 17

---
+++ src/main/java/io/github/akhabali/ApplicationEntryPoint.java
@@ -0,0 +1,13 @@
+package io.github.akhabali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationEntryPoint {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationEntryPoint.class, args);
+	}
+
+}
